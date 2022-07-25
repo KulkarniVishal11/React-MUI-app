0e9c033c2d08c5767372f6768aab41dc65df37cd
Vishal Kulkarni <kulkarni.vishal@ltts.com>
2022-07-25 16:46:25 +0530

    Learned about the button and IconButton and typography and Varient and sx props

--- src/App.js
+++ src/App.js
@@ -1,9 +1,49 @@
-
-
+import { Button, IconButton, Typography } from "@mui/material";
+import SettingsIcon from "@mui/icons-material/Settings";
+import FingerprintIcon from "@mui/icons-material/Fingerprint";
+import { color, margin } from "@mui/system";
 function App() {
   return (
     <div className="App">
-      Hello
+      <Button variant="text">Text</Button>
+      <Button
+        startIcon={<SettingsIcon />}
+        variant="contained"
+        color="secondary"
+      >
+        Contained
+      </Button>
+      <Button
+        variant="outlined"
+        disabled
+        size="small"
+        endIcon={<SettingsIcon />}
+      >
+        Outlined
+      </Button>
+      <IconButton>
+        <FingerprintIcon />
+      </IconButton>
+
+      <Typography variant="h1" component="p">
+        It use the h1 style but its a p tag
+      </Typography>
+
+      <Button
+        variant="contained"
+        sx={{
+          background: "skyblue",
+          color: "black",
+          margin: "20px",
+          padding: "10px",
+          "&:hover": {
+            background: "black",
+            color: "white",
+          },
+        }}
+      >
+        custom color
+      </Button>
     </div>
   );
 }
