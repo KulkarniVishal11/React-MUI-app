d55ab103792d3d5ed218db26e914248301798787
Vishal Kulkarni <kulkarni.vishal@ltts.com>
2022-07-26 10:01:49 +0530

    Added the component for leftside , feed, rightside and navbar and used the stack component

--- src/App.js
+++ src/App.js
@@ -1,66 +1,19 @@
-import { Button, IconButton, styled, Typography } from "@mui/material";
-import SettingsIcon from "@mui/icons-material/Settings";
-import FingerprintIcon from "@mui/icons-material/Fingerprint";
+import { Box, Stack } from "@mui/material";
+import Feedbar from "./Components/Feedbar";
+import { Navbar } from "./Components/Navbar";
+import Rightbar from "./Components/Rightbar";
+import Sidebar from "./Components/Sidebar";
+
 function App() {
-  const Customecolor = styled(Button)(({theme})=>({
-    background: theme.palette.primary.main,
-    color: "black",
-    margin: "20px",
-    padding: "10px",
-    "&:hover": {
-      background: "black",
-      color: "white",
-    },
-  }));
-
   return (
-    <div className="App">
-      <Button variant="text">Text</Button>
-      <Button
-        startIcon={<SettingsIcon />}
-        variant="contained"
-        color="otherColor"
-      >
-        Contained
-      </Button>
-      <Button
-        variant="outlined"
-        disabled
-        size="small"
-        endIcon={<SettingsIcon />}
-      >
-        Outlined
-      </Button>
-      <IconButton>
-        <FingerprintIcon />
-      </IconButton>
-
-      <Typography variant="h1" component="p">
-        It use the h1 style but its a p tag
-      </Typography>
-
-      <Button
-        variant="contained"
-        sx={{
-          background: "skyblue",
-          color: "black",
-          margin: "20px",
-          padding: "10px",
-          "&:hover": {
-            background: "black",
-            color: "white",
-          },
-        }}
-      >
-        custom color
-      </Button>
-
-
-        <Customecolor>
-          Customecolor
-        </Customecolor>
-      {/* Now if you see that we are writing the code and customizing the color of button beside doing that what we can do create our own custome button*/}
-    </div>
+    <Box>
+      {/* <Navbar/> */}
+      <Stack direction='row' spacing={2} justifyContent='space-between'>
+        <Sidebar />
+        <Feedbar />
+        <Rightbar />
+      </Stack>
+    </Box>
   );
 }
 
