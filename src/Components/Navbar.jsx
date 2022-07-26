144fce9b3a8cbadf63c5e0d082be529da1e20982
Vishal Kulkarni <kulkarni.vishal@ltts.com>
2022-07-26 16:00:40 +0530

    Added the style to the navbar and side bar

--- src/Components/Navbar.jsx
+++ src/Components/Navbar.jsx
@@ -1,7 +1,89 @@
-import React from 'react'
+import {
+  AppBar,
+  Avatar,
+  Badge,
+  InputBase,
+  Menu,
+  MenuItem,
+  styled,
+  Toolbar,
+  Typography,
+} from "@mui/material";
+import React,{useState} from "react";
+import SentimentSatisfiedAltIcon from "@mui/icons-material/SentimentSatisfiedAlt";
+import { Box } from "@mui/material";
+import { Mail, Notifications } from "@mui/icons-material";
 
 export const Navbar = () => {
+  const StyledToolbar = styled(Toolbar)({
+    display: "flex",
+    justifyContent: "space-between",
+  });
+
+  const Search = styled("div")(({ theme }) => ({
+    backgroundColor: "white",
+    padding: "0 10px",
+    borderRadius: theme.shape.borderRadius,
+    width: "38%",
+  }));
+
+  const Icons = styled(Box)((theme) => ({
+    display: "flex",
+    gap: "20px",
+    alignItems: "center",
+   
+  }));
+
+  // const UserBox = styled(Box)((theme) => ({
+  //   display: "flex",
+  //   gap: "10px",
+  //   alignItems: "center",
+   
+  // }));
+  const [openMenu, setOpenMenu] = useState(false)
   return (
-    <div>Navbar</div>
-  )
-}
+    <AppBar position="sticky">
+      <StyledToolbar>
+        <Typography variant="h6" sx={{ display: { xs: "none", sm: "block" } }}>
+          VKGRAM
+        </Typography>
+        <SentimentSatisfiedAltIcon
+          sx={{ display: { xs: "block", sm: "none" } }}
+        />
+        <Search>
+          <InputBase placeholder="Search..." />
+        </Search>
+        <Icons>
+          <Badge badgeContent={4} color="error">
+            <Mail />
+          </Badge>
+          <Badge badgeContent={4} color="error">
+            <Notifications />
+          </Badge>
+          <Avatar sx={{ width: 30, height: 30 }} onClick={e=>setOpenMenu(true)}/ >
+        </Icons>
+      </StyledToolbar>
+{/* Menu bar */}
+
+<Menu
+        id="demo-positioned-menu"
+        aria-labelledby="demo-positioned-button"
+
+        open={openMenu}
+        onClose={e=>setOpenMenu(false)}  
+        anchorOrigin={{
+          vertical: 'top',
+          horizontal: 'right',
+        }}
+        transformOrigin={{
+          vertical: 'top',
+          horizontal: 'right',
+        }}
+      >
+        <MenuItem >Profile</MenuItem>
+        <MenuItem >My account</MenuItem>
+        <MenuItem >Logout</MenuItem>
+      </Menu>
+    </AppBar>
+  );
+};
--- src/Components/Sidebar.jsx
+++ src/Components/Sidebar.jsx
@@ -1,15 +1,90 @@
-import { Box } from "@mui/material";
+import {
+  Box,
+  List,
+  ListItem,
+  ListItemButton,
+  ListItemIcon,
+  ListItemText,
+  Switch,
+} from "@mui/material";
 import React from "react";
-
+import HomeIcon from "@mui/icons-material/Home";
+import ArticleIcon from '@mui/icons-material/Article';
+import GroupsIcon from '@mui/icons-material/Groups';
+import StorefrontIcon from '@mui/icons-material/Storefront';
+import GroupIcon from '@mui/icons-material/Group';
+import SettingsIcon from '@mui/icons-material/Settings';
+import AccountBoxIcon from '@mui/icons-material/AccountBox';
+import DarkModeIcon from '@mui/icons-material/DarkMode';
 const Sidebar = () => {
   return (
-    <Box
-      bgcolor="lightcoral"
-      flex={1}
-      p={2}
-      sx={{ display: { xs: "none", sm: "block" } }}
-    >
-      Sidebar
+    <Box flex={1} p={2} sx={{ display: { xs: "none", sm: "block" } }}>
+      <List>
+        <ListItem disablePadding>
+          <ListItemButton>
+            <ListItemIcon>
+              <HomeIcon />
+            </ListItemIcon>
+            <ListItemText primary="HomePage" />
+          </ListItemButton>
+        </ListItem>
+        <ListItem disablePadding>
+          <ListItemButton>
+            <ListItemIcon>
+              <ArticleIcon />
+            </ListItemIcon>
+            <ListItemText primary="Pages" />
+          </ListItemButton>
+        </ListItem>
+        <ListItem disablePadding>
+          <ListItemButton>
+            <ListItemIcon>
+              <GroupsIcon />
+            </ListItemIcon>
+            <ListItemText primary="Groups" />
+          </ListItemButton>
+        </ListItem>
+        <ListItem disablePadding>
+          <ListItemButton>
+            <ListItemIcon>
+              <StorefrontIcon />
+            </ListItemIcon>
+            <ListItemText primary="Market" />
+          </ListItemButton>
+        </ListItem>
+        <ListItem disablePadding>
+          <ListItemButton>
+            <ListItemIcon>
+              <GroupIcon />
+            </ListItemIcon>
+            <ListItemText primary="Friend" />
+          </ListItemButton>
+        </ListItem>
+        <ListItem disablePadding>
+          <ListItemButton>
+            <ListItemIcon>
+              <SettingsIcon />
+            </ListItemIcon>
+            <ListItemText primary="Setting" />
+          </ListItemButton>
+        </ListItem>
+        <ListItem disablePadding>
+          <ListItemButton>
+            <ListItemIcon>
+              <AccountBoxIcon />
+            </ListItemIcon>
+            <ListItemText primary="Profile" />
+          </ListItemButton>
+        </ListItem>
+        <ListItem disablePadding>
+          <ListItemButton>
+            <ListItemIcon>
+              <DarkModeIcon />
+            </ListItemIcon>
+            <Switch color="secondary"/>
+          </ListItemButton>
+        </ListItem>
+      </List>
     </Box>
   );
 };
